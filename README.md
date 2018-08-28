1b339f2458559a400d24ae2255600f49ae9d3294
MR4 <37078885+kerambit5005@users.noreply.github.com>
2018-08-28 19:19:43 +0800

    data

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# THE-fake
-ntahh
+# THIS TOOL COPY DARIPADA CYBER SORA TEAM
+
+The Fake is spammer tools defacer.id
+You need cpanel here.
+
+[+] Make Wildcard Domain First
+1. Login To Cpanel
+2. Make Subdomain (called *)
+Ex: *.yourdomain.com
+
+Setup The-Fake:
+$git clone https://github.com/soracyberteam/the-fake/
+$cd the-fake
+$chmod 777 setup.sh
+$./setup.sh
+
+Run The-Fake:
+$./fake.sh
+Choose number 1 option (Generate Subdomain)
+Insert your domain. Ex: yourdomain.com
+Output? just type "test.txt"
+And generate the subdo
+Open a new session
+Choose number 2 option (Mass Submit)
+Insert your nick, team, and file
+file is (test.txt)
+And Submit !!
+Now Just Check http://defacer.id
+[+] Happy Spammer!
---
+++ fake.sh
@@ -0,0 +1,281 @@
+#!/bin/bash
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo " =================== Welcome To ===================";
+echo 'The  _____     _            _____           _ ANONYMOUS MALAYSIA                 ';
+echo '    |  ___|_ _| | _____    |_   _|__   ___ | |___ ';
+echo '    | |_ / _` | |/ / _ \_____| |/ _ \ / _ \| / __|';
+echo '    |  _| (_| |   <  __/_____| | (_) | (_) | \__ \';
+echo '    |_|  \__,_|_|\_\___|     |_|\___/ \___/|_|__/ v.3.0';
+echo ' ---------------- Coded By MR4azwar/MR4_MALAYSIA ---------------';
+echo '';
+echo 'Nb:  Tools Ini!';
+echo '    SUBCRIB€ TO ANONYMOUs MALAYSIA  OFFICIAL'; 
+echo '';
+echo 'Pilihlah sial:';
+echo '[1] Generate Subdomain';
+echo '[2] Mass Submit Def.ID';
+echo '[3] Auto Tebas (Spoof)';
+echo ''
+read -p "fake@tools:~#" command
+if [ $command -eq 1 ];
+	then
+		cd data/ && chmod +x subdo.sh && ./subdo.sh
+elif [ $command -eq 2 ];
+	then
+		cd data/ && chmod +x submit.sh && ./submit.sh
+elif [ $command -eq 3 ];
+	then
+		cd data/ && chmod +x spoof.sh && ./spoof.sh
+fi
