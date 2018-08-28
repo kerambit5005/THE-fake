140773130eb636b38942cfc0f335b76c8150fc41
MR4 <37078885+kerambit5005@users.noreply.github.com>
2018-08-28 19:22:15 +0800

    Add files via upload

---
+++ subdo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+echo " =================== Welcome To ===================";
+echo 'The  _____     _            _____           _     ';
+echo '    |  ___|_ _| | _____    |_   _|__   ___ | |___ ';
+echo '    | |_ / _` | |/ / _ \_____| |/ _ \ / _ \| / __|';
+echo '    |  _| (_| |   <  __/_____| | (_) | (_) | \__ \';
+echo '    |_|  \__,_|_|\_\___|     |_|\___/ \___/|_|___/ v.3.0';
+echo ' ---------------- Coded By MR4azwar ---------------';
+echo '';
+read -p "Domain : http://" domain
+read -p "Delay? (1-5 ) :" delay
+read -p "Jumlah? (1-10000):" jumlah
+read -p "Output (test.txt) :" output
+echo "[+] Generating..."
+count=1
+while [ $count -le $jumlah ]
+do
+printf "http://$RANDOM.$domain\n" >> $output
+echo "[$count] Generated => data/$output"
+sleep $delay
+(( count++ ))
+done
+echo "[+] Success"
+
